9485830542545afd45b9772782432b65c32bc5e7
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-06-29 01:02:40 -0700

    pattern2

---
+++ Patterns/pattern2.cpp
@@ -0,0 +1,27 @@
+// Triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+ // Output
+
+// *
+// **
+// ***
+// ****
