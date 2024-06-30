d6f50f47f401c11f4107770c2fc0a58e82a7a9a6
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-06-30 15:32:44 +0500

    Solved Pattern 12

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -296,15 +296,57 @@ void pattern11(int n)
         }
         cout << endl;
     }
+
+    // Output 
+
+    // 1
+    // 01
+    // 010
+    // 1010
+    // 10101
+}
+
+// Pattern 12
+
+void pattern12(int n)
+{
+    int space = 2 * (n-1);
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+
+        for(int j = 1; j<= space; j++)
+        {
+            cout <<" ";
+        }
+
+        for(int j = i; j>= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+        space -= 2;
+    }
+    
+    // Output
+
+    // 1        1
+    // 12      21
+    // 123    321
+    // 1234  4321
+    // 1234554321
 }
 
 int main()
 {
     int n;
-    // cout << "Enter value of N : ";
-    // cin >> n;
-    n = 5;
-    pattern11(n);
+    cout << "Enter value of N : ";
+    cin >> n;
+
+    pattern12(n);
 
     return 0;
 }
