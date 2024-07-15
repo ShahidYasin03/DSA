ec13d3b05d1922ea5df8c5abc2e072a36920967c
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-07-15 14:54:30 +0500

    BinaryTree

---
+++ Tree/createTree.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *right;
+    node *left;
+
+    node(int data)
+    {
+        this->data = data;
+        this->right = nullptr;
+        this->left = nullptr;
+    }
+};
+
+node *buildTree(node *root)
+{
+    cout << "Enter data" << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    if (data == -1)
+    {
+        return nullptr;
+    }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+void levelOrder(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }else{
+        cout << temp->data << "   ";
+
+        if (temp->left)
+        {
+            q.push(temp->left);
+        }
+
+        if (temp->right)
+        {
+            q.push(temp->right);
+        }
+
+        }
+    }
+}
+
+int main()
+{
+    node *root = nullptr;
+
+    root = buildTree(root);
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    levelOrder(root);
+    return 0;
+}
