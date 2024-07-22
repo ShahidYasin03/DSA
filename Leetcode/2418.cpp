0bc1e2944af21783d9bb6d87424a3f07dc715879
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-07-22 07:33:47 +0500

    Solved leetcode 2418

---
+++ Leetcode/2418.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        const int n=names.size();
+        vector<pair<int, string>> hn(n);
+        for(int i=0; i<n; i++) 
+            hn[i]={heights[i], names[i]};
+        sort(hn.begin(), hn.end(), greater<>());
+        for(int i=0; i<n; i++) 
+            names[i]=hn[i].second;
+        return names;    
+    }
+};
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
