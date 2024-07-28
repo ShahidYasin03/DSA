7181e225f7ba81b4f497295f93786c946862ed7f
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-07-28 07:09:25 +0500

    Solved LeetCode 5

--- Leetcode/5.cpp
+++ Leetcode/5.cpp
@@ -1,46 +1,62 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Solution
 {
 public:
-    bool checkP(string s)
-    {
-        // checks is string is palindrome
-        int n = s.length();
-        for (int i = 0; i < n / 2; i++)
-        {
-            if (s[i] != s[n - i - 1])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
     string longestPalindrome(string s)
     {
-        string result = "";
-        for (int i = 0; i < s.length(); i++)
+        string res = "";
+        int resLen = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++)
         {
-            string x = "";
-            for (int j = i; j < s.length(); j++)
+            // Odd-length palindromes
+            int r = i, l = i;
+            while (l >= 0 && r < n && s[l] == s[r])
             {
-                x += s[j];
-                if (checkP(x))
+                if ((r - l + 1) > resLen)
                 {
-                    result = (result.length() < x.length()) ? x : result;
+                    res = s.substr(l, r - l + 1); 
+                    resLen = (r - l + 1);
                 }
+                l--;
+                r++;
+            }
+
+            // Even-length palindromes
+            r = i + 1, l = i;
+            while (l >= 0 && r < n && s[l] == s[r])
+            {
+                if ((r - l + 1) > resLen)
+                {
+                    res = s.substr(l, r - l + 1); 
+                    resLen = (r - l + 1);
+                }
+                l--;
+                r++;
             }
         }
 
-        return result;
+        return res;
     }
 };
 
 int main()
 {
-    Solution sol;
-    string s = "borcdubqiupahpwjizjjbkncelfazeqynfbrnzuvbhjnyvrlkdyfyjjxnprfocrmisugizsgvhszooktdwhehojbrdbtgkiwkhpfjfcstwcajiuediebdhukqnroxbgtvottummbypokdljjvnthcbesoyigscekrqswdpalnjnhcnqrarxuufzzmkwizptyvjhpfidgmskuaggtpxqisdlyloznkarxofzeeyvteynluofuhbllyiszszbwnsglqjkignusarxsjvctpgiwnhdufmfpanfwxjwlmhgllzsmdpncbwnsbdtsvrjybynifywkulqnzprcxockbhrhvnwxrkvwumyieazclcviumvormynbryaziijpdinwatwqppamfiqfiojgwkvfzyxadyfjrgmtttvlgkqghgbcfhkigfojjkhskzenlpasyozcsuccdxhulcubsgomvcrbqwakrraesfifecmoztayrdjicypakrrneulfbqqxtrdelggedvloebqaztmfyfkhuonrognejxpesmwgnmnnnamvkxqvzrgzdqtvuhccryeowywmbixktnkqnwzlzfvloyqcageugmjqihyjxhssmhkfzxpzxmgpjgsduogfolnkkqizitbvvgrkczmojxnabruwwzxxqcevdwvtiigwckpxnnxxxdhxpgomncttjutrsvyrqcfwxhexhaguddkiesmfrqfjfdaqbkeqinwicphktffuwcazlmerdhraufbpcznbuipmymipxbsdhuesmcwnkdvilqbnkaglloswcpqzdggnhjdkkumfuzihilrpcvemwllicoqiugobhrwdxtoefynqmccamhdtxujfudbglmiwqklriolqfkknbmindxtljulnxhipsieyohnczddabrxzjmompbtnnxvivmoyfzfrxlufowtqzojfclmatthjtbhotdoheusnpirwicbtyrcuojsdmfcx";
-    cout << sol.longestPalindrome(s) << endl;
+    Solution solution;
+
+    // Test case 1
+    string s1 = "babad";
+    cout << "Input: " << s1 << endl;
+    cout << "Longest Palindromic Substring: " << solution.longestPalindrome(s1) << endl;
+
+    // Test case 2
+    string s2 = "cbbd";
+    cout << "Input: " << s2 << endl;
+    cout << "Longest Palindromic Substring: " << solution.longestPalindrome(s2) << endl;
+
     return 0;
 }
