3d111edc1eb90b6301da7c30401b630bc1fc1e2c
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-08-10 01:27:18 +0500

    Solved LeetCode 704

--- Leetcode/704.cpp
+++ Leetcode/704.cpp
@@ -15,7 +15,7 @@ public:
     int search(vector<int>& nums, int target) {
         int s = 0, e = nums.size() - 1;
         while (s <= e) {
-            int mid = (s + e) / 2;
+            int mid = s + (e + s) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
