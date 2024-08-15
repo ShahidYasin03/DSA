592a23492f96394c6d2b10dddd1a3e48adb4c28e
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-08-15 19:08:02 +0500

    LeetCode 860

---
+++ Leetcode/860.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+auto init = [](){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+cout.tie(nullptr);
+return 'c';
+}();
+class Solution
+{
+public:
+    bool lemonadeChange(vector<int> &bills)
+    {
+        int five = 0, ten = 0, twen = 0;
+
+        for (int i = 0; i < bills.size(); i++)
+        {
+            if (bills[i] == 5)
+            {
+                five++;
+            }
+            else if (bills[i] == 10)
+            {
+                ten++;
+                if (five)
+                {
+                    five--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                twen++;
+                if (ten && five)
+                {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3)
+                {
+                    five -= 3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+int main()
+{
+
+    Solution sol;
+    vector<int> x {5, 5, 5, 10, 20};
+    cout << boolalpha;
+    cout << sol.lemonadeChange(x) << endl;
+
+    return false;
+}
