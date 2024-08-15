a2dc86965a1d1d5b703838fea7f562aaca5bfa1a
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-08-15 23:04:10 +0500

    15

--- Leetcode/15.cpp
+++ Leetcode/15.cpp
@@ -3,6 +3,13 @@
 #include <algorithm>
 using namespace std;
 
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
 class Solution
 {
 public:
