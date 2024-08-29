9affd2f1af4366b0db34fb6dafe7f1d42c14d805
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-08-30 01:49:58 +0500

    LeetCode 150

---
+++ LeetCode-150/172.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum = 0;
+        while(n / 5 > 0)
+        {
+            sum += (n / 5);
+            n /= 5;
+        }
+        return sum;
+    }
+};
+int main()
+{
+    Solution sol;
+
+    cout << "Trailings zeros are : "<< sol.trailingZeroes(150)<< endl;
+    return 0;
+}
