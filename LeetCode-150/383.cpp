8fdfd116923b33506b3e31f816cddbb636e28ed1
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-08-31 01:09:55 +0500

    Mapy

--- LeetCode-150/383.cpp
+++ LeetCode-150/383.cpp
@@ -1,32 +1,57 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 class Solution
 {
 public:
     bool canConstruct(string ran, string mag)
     {
-        sort(ran.begin(), ran.end());
-        sort(mag.begin(), mag.end());
-        int l = 0, r = 0;
-        while (l < ran.length() && r < mag.length())
-        {
 
-            if (ran[l] > mag[r])
+        // Sorting
+
+        // sort(ran.begin(), ran.end());
+        // sort(mag.begin(), mag.end());
+        // int l = 0, r = 0;
+        // while (l < ran.length() && r < mag.length())
+        // {
+
+        //     if (ran[l] > mag[r])
+        //     {
+        //         r++;
+        //         continue;
+        //     }
+        //     else if (ran[l] != mag[r])
+        //     {
+        //         return false;
+        //     }
+        //     l++;
+        //     r++;
+        // }
+        // if(l < ran.length())
+        // {return false;}
+        // return true;
+
+        //Map
+        
+        unordered_map<char, int> mapy;
+        for (int i = 0; i < mag.length(); i++)
+        {
+            mapy[mag[i]]++;
+        }
+        for (int i = 0; i < ran.length(); i++)
+        {
+            char c = ran[i];
+            if (mapy.find(c) != mapy.end() && mapy[c] > 0)
             {
-                r++;
-                continue;
+                mapy[c]--;
             }
-            else if (ran[l] != mag[r])
+            else
             {
                 return false;
             }
-            l++;
-            r++;
         }
-        if(l < ran.length())
-        {return false;}
         return true;
     }
 };
@@ -34,8 +59,8 @@ public:
 int main()
 {
     Solution sol;
-    string ransom = "fihjjjjei";
-    string magzine = "hjibagacbhadfaefdjaeaebgi";
+    string ransom = "aa";
+    string magzine = "ab";
 
     cout << sol.canConstruct(ransom, magzine) << endl;
     return 0;
