63587fb6ed267f61b9dec410c3656e414dbf13e8
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-09-10 00:42:44 +0500

    LeetCOde 2326

--- Leetcode/2326.cpp
+++ Leetcode/2326.cpp
@@ -13,45 +13,43 @@ struct ListNode {
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<int> x;
-        ListNode* temp = head;
-        while(temp)
-        {
-            x.push_back(temp->val);
-            temp = temp->next;
-        }  
+        ListNode* temp = head;  
         vector<vector<int>> vec(m, vector<int>(n, -1));
         int direction = 0;
         int top = 0, left = 0, right = n - 1, bottom = m - 1;
         int index = 0;
-        while(top <= bottom && left <= right && index < x.size())
+        while(top <= bottom && left <= right && temp != nullptr)
         {
             if(direction == 0)
             {
-                for(int i = left; i <= right && index < x.size(); i++)
+                for(int i = left; i <= right && temp != nullptr; i++)
                 {
-                    vec[top][i] = x[index++];
+                    vec[top][i] = temp->val;
+                    temp = temp->next;
                 }
                 top++;
             }else if(direction == 1)
             {
-                for(int i = top; i <= bottom && index < x.size(); i++)
+                for(int i = top; i <= bottom && temp != nullptr; i++)
                 {
-                    vec[i][right] = x[index++];
+                    vec[i][right] = temp->val;
+                    temp = temp->next;
                 }
                 right--;
             }else if(direction == 2)
             {
-                for(int i = right; i >= left && index < x.size(); i--)
+                for(int i = right; i >= left && temp != nullptr; i--)
                 {
-                    vec[bottom][i] = x[index++];
+                    vec[bottom][i] = temp->val;
+                    temp = temp->next;
                 }
                 bottom--;
             }else if(direction == 3)
             {
-                for(int i = bottom; i >= top && index < x.size(); i--)
+                for(int i = bottom; i >= top && temp != nullptr; i--)
                 {
-                    vec[i][left] = x[index++];
+                    vec[i][left] = temp->val;
+                    temp = temp->next;
                 }
                 left++;
             }
