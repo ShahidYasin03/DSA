be615e6ccfa0dcdf83e43b73a010e07c94a5c38f
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-09-17 16:35:32 +0500

    LeetCode 6 Update

--- Leetcode/6.cpp
+++ Leetcode/6.cpp
@@ -7,45 +7,77 @@ class Solution
 public:
     string convert(string s, int numRows)
     {
-        if(s.length() <= 1 || numRows <= 1)
+        if (numRows <= 1 || s.length() <= 1)
         {
             return s;
         }
-        string ans = "";
-        int n = numRows, m = 7;
-        vector<vector<char>> x(n, vector<char>(m, '*'));
-        int index = 0;
-        int i = 0, j = 0;
-        while(index < s.length())
+
+        // Vector of strings to store characters row-wise
+        vector<string> rows(min(numRows, (int)s.size()));
+        int currentRow = 0;
+        bool goingDown = false;
+
+        for (char c : s)
         {
-            while(i < numRows)
+            rows[currentRow] += c;
+            if (currentRow == 0 || currentRow == numRows - 1)
             {
-                x[i][j] = s[index++];
-                i++;
+                goingDown = !goingDown; // Change direction
             }
-            i--;
-            while(i > 0)
-            {
-                i--;
-                j++;
-                x[i][j] = s[index++];
-            }
-            i++;
+            currentRow += goingDown ? 1 : -1;
         }
-        for(int k = 0; k < n; k++)
+
+        // Concatenate all rows to get the final result
+        string ans;
+        for (string row : rows)
         {
-            for(int l = 0; l < m; l++)
-            {
-                // cout << x[k][l] << "    ";
-                if(x[k][l] != '*')
-                {
-                    ans += x[k][l];
-                }
-            }
-            // cout << endl;
+            ans += row;
         }
+
         return ans;
     }
+
+    // string convert(string s, int numRows)
+    // {
+    //     if(s.length() <= 1 || numRows <= 1)
+    //     {
+    //         return s;
+    //     }
+    //     string ans = "";
+    //     int n = numRows, m = 7;
+    //     vector<vector<char>> x(n, vector<char>(m, '*'));
+    //     int index = 0;
+    //     int i = 0, j = 0;
+    //     while(index < s.length())
+    //     {
+    //         while(i < numRows)
+    //         {
+    //             x[i][j] = s[index++];
+    //             i++;
+    //         }
+    //         i--;
+    //         while(i > 0)
+    //         {
+    //             i--;
+    //             j++;
+    //             x[i][j] = s[index++];
+    //         }
+    //         i++;
+    //     }
+    //     for(int k = 0; k < n; k++)
+    //     {
+    //         for(int l = 0; l < m; l++)
+    //         {
+    //             // cout << x[k][l] << "    ";
+    //             if(x[k][l] != '*')
+    //             {
+    //                 ans += x[k][l];
+    //             }
+    //         }
+    //         // cout << endl;
+    //     }
+    //     return ans;
+    // }
 };
 
 int main()
