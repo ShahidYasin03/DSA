89ad182482074216bd27c0f5227a572bc93e3642
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-09-21 00:23:01 +0500

    Update

--- Leetcode/73.cpp
+++ Leetcode/73.cpp
@@ -1,81 +1,114 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 using namespace std;
-class Solution
-{
+
+// OPtimized solution with extra space
+class Solution {
 public:
-    void setZeroes(vector<vector<int>> &matrix)
-    {
+    void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        bool flag = false;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (matrix[i][j] == 0)
-                {
-                    flag = true;
-                    if (j + 1 < m)
-                    {
-                        for (int k = j + 1; k < m; k++)
-                        {
-                            if (matrix[i][k] != 0)
-                            {
-                                matrix[i][k] = -69;
-                            }
-                        }
-                    }
+        unordered_set<int> zeroRows, zeroCols;
 
-                    if (j - 1 >= 0)
-                    {
-                        for (int k = j - 1; k >= 0; k--)
-                        {
-                            if (matrix[i][k] != 0)
-                            {
-                                matrix[i][k] = -69;
-                            }
-                        }
-                    }
-
-                    if (i + 1 < n)
-                    {
-                        for (int k = i + 1; k < n; k++)
-                        {
-                            if (matrix[k][j] != 0)
-                            {
-                                matrix[k][j] = -69;
-                            }
-                        }
-                    }
-
-                    if (i - 1 >= 0)
-                    {
-                        for (int k = i - 1; k >= 0; k--)
-                        {
-                            if (matrix[k][j] != 0)
-                            {
-                                matrix[k][j] = -69;
-                            }
-                        }
-                    }
+        // First pass: identify all rows and columns that should be zeroed
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows.insert(i);
+                    zeroCols.insert(j);
                 }
             }
         }
-        if (flag)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    if (matrix[i][j] == -69)
-                    {
-                        matrix[i][j] = 0;
-                    }
+
+        // Second pass: set matrix elements to zero based on identified rows and columns
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (zeroRows.count(i) || zeroCols.count(j)) {
+                    matrix[i][j] = 0;
                 }
             }
         }
     }
 };
+
+
+//My Brute Force Solution
+
+// class Solution
+// {
+// public:
+//     void setZeroes(vector<vector<int>> &matrix)
+//     {
+//         int n = matrix.size(), m = matrix[0].size();
+//         bool flag = false;
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = 0; j < m; j++)
+//             {
+//                 if (matrix[i][j] == 0)
+//                 {
+//                     flag = true;
+//                     if (j + 1 < m)
+//                     {
+//                         for (int k = j + 1; k < m; k++)
+//                         {
+//                             if (matrix[i][k] != 0)
+//                             {
+//                                 matrix[i][k] = -69;
+//                             }
+//                         }
+//                     }
+
+//                     if (j - 1 >= 0)
+//                     {
+//                         for (int k = j - 1; k >= 0; k--)
+//                         {
+//                             if (matrix[i][k] != 0)
+//                             {
+//                                 matrix[i][k] = -69;
+//                             }
+//                         }
+//                     }
+
+//                     if (i + 1 < n)
+//                     {
+//                         for (int k = i + 1; k < n; k++)
+//                         {
+//                             if (matrix[k][j] != 0)
+//                             {
+//                                 matrix[k][j] = -69;
+//                             }
+//                         }
+//                     }
+
+//                     if (i - 1 >= 0)
+//                     {
+//                         for (int k = i - 1; k >= 0; k--)
+//                         {
+//                             if (matrix[k][j] != 0)
+//                             {
+//                                 matrix[k][j] = -69;
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         if (flag)
+//         {
+//             for (int i = 0; i < n; i++)
+//             {
+//                 for (int j = 0; j < m; j++)
+//                 {
+//                     if (matrix[i][j] == -69)
+//                     {
+//                         matrix[i][j] = 0;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// };
 
 int main()
 {
