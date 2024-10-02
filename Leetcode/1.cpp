9efaab36e84065ea6b41c409753fd530c8d9e170
ShahidYasin03 <shahidyasin1st@gmail.com>
2024-10-02 11:01:54 +0500

    Leetcode start

--- Leetcode/1.cpp
+++ Leetcode/1.cpp
@@ -8,43 +8,59 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        vector<pair<int, int>> m;
-        int n = nums.size();
-        for (int i = 0; i < n; i++)
+        vector<int> num = nums; // Copy the original array
+        sort(num.begin(), num.end()); // Sort the array for binary search
+        int x = -1, y = -1;
+        // Find two numbers whose sum equals the target
+        for (int i = 0; i < num.size() - 1; i++)
         {
-            m.push_back({nums[i], i}); 
+            int j = i + 1;
+            int end = num.size() - 1;
+            while (j <= end)
+            {
+                int mid = (j + end) / 2;
+                if (num[i] + num[mid] == target)
+                {
+                    x = num[i]; // Store the numbers found
+                    y = num[mid];
+                    break; // Break out of the loop if found
+                }
+                else if (num[i] + num[mid] < target)
+                {
+                    j = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1; // Correct update for the `end` index
+                }
+            }
+            if (x != -1 && y != -1) // Exit the loop early once pair is found
+                break;
         }
 
-       
-        sort(m.begin(), m.end());
-
-        int i = 0, j = n - 1;
-        while (i < j) 
+        // Finding the original indices of the two numbers in the original array
+        int et = -1, ed = -1;
+        for (int i = 0; i < nums.size(); i++)
         {
-            int sum = m[i].first + m[j].first;
-            if (sum == target)
+            if (x == nums[i] && et == -1)
             {
-                return {m[i].second, m[j].second};
+                et = i; // First occurrence of x
             }
-            else if (sum > target)
+            else if (y == nums[i] && ed == -1)
             {
-                j--;
-            }
-            else
-            {
-                i++;
+                ed = i; // First occurrence of y
             }
         }
-        return {};
+        return {et, ed};
     }
 };
 
 int main()
 {
-    vector<int> x {3, 2, 4};
+    vector<int> x{3, 2, 4, 7, 9};
 
     Solution sol;
-    vector<int> ans = sol.twoSum(x, 6);
+    vector<int> ans = sol.twoSum(x, 9);
 
     cout << ans[0] << ", " << ans[1] << endl;
 
