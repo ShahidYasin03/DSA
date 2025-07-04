6c286aca6ee6016f6eedf9a40da912b388fd6316
ShahidYasin03 <shahidyasin1st@gmail.com>
2025-07-05 01:24:41 +0500

    ---69---

--- Leetcode/69.cpp
+++ Leetcode/69.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         if (x == 0) return 0;
+//         long long i = 0;
+//         long long ans = 0;
+//         while (ans <= x) {
+//             ans = i * i;
+//             i++;
+//         }
+//         return i - 2;
+//     }
+// };
+
+class Solution
+{
 public:
-    int mySqrt(int x) {
-        if (x == 0) return 0;
-        long long i = 0;
-        long long ans = 0;
-        while (ans <= x) {
-            ans = i * i;
-            i++;
+    int mySqrt(int x)
+    {
+        if (x < 2)
+            return x;
+
+        int left = 1, right = x / 2, ans = 0;
+        while (left <= right)
+        {
+            long long mid = left + (right - left) / 2;
+            long long square = mid * mid;
+
+            if (square == x)
+            {
+                return mid;
+            }
+            else if (square < x)
+            {
+                ans = mid;
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
         }
-        return i - 2;
+        return ans;
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
 
     int test1 = 4;
