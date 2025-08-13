a9484a247a837db26b14a79bce96b6c08864bd32
ShahidYasin03 <shahidyasin1st@gmail.com>
2025-08-13 13:22:45 +0500

    resubmit 852

--- Leetcode/852.cpp
+++ Leetcode/852.cpp
@@ -2,20 +2,29 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        for(int i = 0; i < arr.size()-1; i++)
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int s = 0;
+        int e = arr.size()-1;
+        while(s < e)
         {
-            if(arr[i] > arr[i+1]) return i;
+            int mid = s + (e-s)/2;
+            if(arr[mid] > arr[mid + 1])
+            {
+                e = mid;
+            }else {
+                s = mid+1;
+            }
         }
-        return -1;
-        
+        return s;
     }
 };
 
-
-int main() {
+int main()
+{
     Solution sol;
     vector<int> test1 = {0, 2, 1, 0};
     vector<int> test2 = {1, 3, 5, 4, 2};
