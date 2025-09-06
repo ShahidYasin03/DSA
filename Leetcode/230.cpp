6f1471f15b80d1569cacfcf26e11224df4e365ac
ShahidYasin03 <shahidyasin1st@gmail.com>
2025-09-06 22:07:42 +0500

    230

--- Leetcode/230.cpp
+++ Leetcode/230.cpp
@@ -9,18 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+// Mine----------------
+// class Solution {
+// public:
+//     void inOrder(vector<int>& num, TreeNode* root)
+//     {
+//         if(!root) return;
+//         inOrder(num, root->left);
+//         num.push_back(root->val);
+//         inOrder(num, root->right);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int> num;
+//         inOrder(num, root);
+//         return num[k-1];
+//     }
+// };
+
+// GPT
+
+class Solution
+{
 public:
-    void inOrder(vector<int>& num, TreeNode* root)
+    void inorder(TreeNode *root, int &k, int &ans)
     {
-        if(!root) return;
-        inOrder(num, root->left);
-        num.push_back(root->val);
-        inOrder(num, root->right);
+        if (!root)
+            return;
+        inorder(root->left, k, ans);
+        k--;
+        if (k == 0)
+        {
+            ans = root->val;
+            return;
+        }
+        inorder(root->right, k, ans);
     }
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> num;
-        inOrder(num, root);
-        return num[k-1];
+
+    int kthSmallest(TreeNode *root, int k)
+    {
+        int ans = -1;
+        inorder(root, k, ans);
+        return ans;
     }
 };
