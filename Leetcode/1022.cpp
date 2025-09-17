8d86f2cf59c2ee093d088e8b5d684ed7c2ed2f9e
ShahidYasin03 <shahidyasin1st@gmail.com>
2025-09-18 03:32:06 +0500

    1022

--- Leetcode/1022.cpp
+++ Leetcode/1022.cpp
@@ -45,3 +45,24 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    int dfs(TreeNode *node, int current)
+    {
+        if (!node)
+            return 0;
+        current = current * 2 + node->val;
+        if (!node->left && !node->right)
+        {
+            return current;
+        }
+        return dfs(node->left, current) + dfs(node->right, current);
+    }
+
+    int sumRootToLeaf(TreeNode *root)
+    {
+        return dfs(root, 0);
+    }
+};
