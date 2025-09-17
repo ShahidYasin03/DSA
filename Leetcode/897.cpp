fa2fed7820e2a766bf98f4e8b66c8216ce7d2b22
ShahidYasin03 <shahidyasin1st@gmail.com>
2025-09-17 14:50:51 +0500

    897

--- Leetcode/897.cpp
+++ Leetcode/897.cpp
@@ -9,29 +9,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+///-----------------------------------sol 1----------------------------
+// class Solution
+// {
+// public:
+//     void inorder(vector<int> &num, TreeNode *root)
+//     {
+//         if (!root)
+//             return;
+//         inorder(num, root->left);
+//         num.push_back(root->val);
+//         inorder(num, root->right);
+//     }
+//     TreeNode *build(vector<int> num, int i)
+//     {
+//         if (i == num.size())
+//             return nullptr;
+//         TreeNode *root = new TreeNode(num[i++]);
+//         root->right = build(num, i);
+//         return root;
+//     }
+//     TreeNode *increasingBST(TreeNode *root)
+//     {
+//         vector<int> num;
+//         inorder(num, root);
+//         return build(num, 0);
+//     }
+// };
+
+// -------------------------- Solution 2 -------------------------------
+
 class Solution
 {
 public:
-    void inorder(vector<int> &num, TreeNode *root)
+    void inorder(TreeNode *&prev, TreeNode *root)
     {
         if (!root)
             return;
-        inorder(num, root->left);
-        num.push_back(root->val);
-        inorder(num, root->right);
+        inorder(prev, root->left);
+        prev->right = new TreeNode(root->val);
+        prev = prev->right;
+        inorder(prev, root->right);
     }
-    TreeNode *build(vector<int> num, int i)
-    {
-        if (i == num.size())
-            return nullptr;
-        TreeNode *root = new TreeNode(num[i++]);
-        root->right = build(num, i);
-        return root;
-    }
+
     TreeNode *increasingBST(TreeNode *root)
     {
-        vector<int> num;
-        inorder(num, root);
-        return build(num, 0);
+        TreeNode *dummy = new TreeNode(-1);
+        TreeNode *prev = dummy;
+        inorder(prev, root);
+        return dummy->right;
     }
 };
