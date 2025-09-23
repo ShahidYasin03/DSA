0b98c5667b5dce89b8258514115f5d2ba41c66d4
ShahidYasin03 <shahidyasin1st@gmail.com>
2025-09-23 18:53:08 +0500

    2418

--- Leetcode/2418.cpp
+++ Leetcode/2418.cpp
@@ -1,43 +1,68 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-class Solution {
+// class Solution {
+// public:
+//     static vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+//         const int n = names.size();
+//         vector<pair<int, string>> hn(n);
+//         for (int i = 0; i < n; i++)
+//             hn[i] = {heights[i], names[i]};
+
+//         sort(hn.begin(), hn.end(), greater<>());
+
+//         for (int i = 0; i < n; i++)
+//             names[i] = hn[i].second;
+
+//         return names;
+//     }
+// };
+
+class Solution
+{
 public:
-    static vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        const int n = names.size();
-        vector<pair<int, string>> hn(n);
-        for (int i = 0; i < n; i++) 
-            hn[i] = {heights[i], names[i]};
-        
-        sort(hn.begin(), hn.end(), greater<>());
-        
-        for (int i = 0; i < n; i++) 
-            names[i] = hn[i].second;
-        
-        return names;    
+    vector<string> sortPeople(vector<string> &names, vector<int> &heights)
+    {
+        priority_queue<pair<int, string>> pq;
+        int n = names.size();
+        for (int i = 0; i < n; i++)
+        {
+            pq.push({heights[i], names[i]});
+        }
+
+        vector<string> ans;
+        while (!pq.empty())
+        {
+            auto temp = pq.top();
+            pq.pop();
+            ans.push_back(temp.second);
+        }
+        return ans;
     }
 };
 
-
-auto init = []() {
+auto init = []()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     return 'c';
 }();
 
-
-int main() {
+int main()
+{
     // Test case 1
+    Solution s;
     vector<string> names1 = {"Alice", "Bob", "Charlie"};
     vector<int> heights1 = {155, 180, 165};
-    vector<string> result1 = Solution::sortPeople(names1, heights1);
+    vector<string> result1 = s.sortPeople(names1, heights1);
     cout << "Test Case 1: ";
-    for (const string& name : result1) {
+    for (const string &name : result1)
+    {
         cout << name << ", ";
     }
     cout << endl; // Expected output: Bob Charlie Alice
@@ -45,14 +70,13 @@ int main() {
     // Test case 2
     vector<string> names2 = {"Tom", "Jerry", "Spike"};
     vector<int> heights2 = {150, 120, 180};
-    vector<string> result2 = Solution::sortPeople(names2, heights2);
+    vector<string> result2 = s.sortPeople(names2, heights2);
     cout << "Test Case 2: ";
-    for (const string& name : result2) {
+    for (const string &name : result2)
+    {
         cout << name << ", ";
     }
     cout << endl; // Expected output: Spike Tom Jerry
 
-
     return 0;
 }
-
