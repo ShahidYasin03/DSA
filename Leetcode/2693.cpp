65b9cd3a42786b1f8e96ca1e8b9115ee05a152ac
ShahidYasin03 <shahidyasin1st@gmail.com>
2025-09-23 15:08:35 +0500

    2693

--- Leetcode/2693.cpp
+++ Leetcode/2693.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+// class Solution
+// {
+// public:
+//     bool isStrictlyPalindromic(int n)
+//     {
+//         return false;
+//     }
+// };
 
 class Solution
 {
 public:
+    bool check(vector<int> num)
+    {
+        int n = num.size() - 1;
+        int i = 0;
+        while (i <= n)
+        {
+            if (num[i] != num[n])
+            {
+                return false;
+            }
+            i++;
+            n--;
+        }
+        return true;
+    }
     bool isStrictlyPalindromic(int n)
     {
-        return false;
+        vector<int> num;
+        for (int i = 2; i <= n - 2; i++)
+        {
+            int x = n;
+            num.clear();
+            while (x)
+            {
+                num.push_back(x % i);
+                x /= i;
+            }
+            if (!check(num))
+                return false;
+        }
+        return true;
     }
 };
 
