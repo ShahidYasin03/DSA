2b988d58e6484b68f18facba2d4911b1376bcb31
ShahidYasin03 <shahidyasin1st@gmail.com>
2025-12-28 14:09:50 +0500

    944

--- Leetcode/944.cpp
+++ Leetcode/944.cpp
@@ -11,9 +11,9 @@ public:
         int count = 0;
         for (int i = 0; i < strs[0].length(); i++)
         {
-            for (int j = 0; j < strs.size(); j++)
+            for (int j = 1; j < strs.size(); j++)
             {
-                if (strs[j][i] < strs[j][i])
+                if (strs[j][i] < strs[j - 1][i])
                 {
                     count++;
                     break;
