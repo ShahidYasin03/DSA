d521426e88a1ab3ac0afc49c21bd902435b65bf6
ShahidYasin03 <shahidyasin1st@gmail.com>
2026-09-20 15:57:09 +0500

    Dutch national flag

--- Python_LeetCode/75.py
+++ Python_LeetCode/75.py
@@ -3,22 +3,39 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero, one = 0, 0
-        for i in nums:
-            if i == 0:
-                zero += 1
-            elif i == 1:
-                one += 1
-        indx = 0
-        while zero:
-            nums[indx] = 0
-            indx += 1
-            zero -= 1
-        while one:
-            nums[indx] = 1
-            indx += 1
-            one -= 1
-        while indx < len(nums):
-            nums[indx] = 2
-            indx += 1
-        
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # zero, one = 0, 0
+        # for i in nums:
+        #     if i == 0:
+        #         zero += 1
+        #     elif i == 1:
+        #         one += 1
+        # indx = 0
+        # while zero:
+        #     nums[indx] = 0
+        #     indx += 1
+        #     zero -= 1
+        # while one:
+        #     nums[indx] = 1
+        #     indx += 1
+        #     one -= 1
+        # while indx < len(nums):
+        #     nums[indx] = 2
+        #     indx += 1
+
+        low = mid = 0 
+        hi = len(nums)-1
+        while mid <= hi:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid +=1
+            else:
+                nums[mid], nums[hi] = nums[hi], nums[mid]
+                hi -= 1
